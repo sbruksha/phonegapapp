64bdafc2eca4ada12f4c132c1199e2a7e8bd1781
sbruksha <sbruksha@gmail.com>
2016-12-09 15:52:53 -0800

    added screens

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-inappbrowser": "1.6.1-dev"
+};
+// BOTTOM OF METADATA
+});
