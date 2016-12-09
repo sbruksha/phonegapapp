5c90cf8712f251775a98f6b9d2fc0d5cb46c5d3d
sbruksha <sbruksha@gmail.com>
2016-12-09 12:24:54 -0800

    added screens

--- www/js/index.js
+++ www/js/index.js
@@ -26,8 +26,8 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
         document.addEventListener('resume', this.onResume, false);
+        document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
@@ -41,7 +41,7 @@ var app = {
     onResume: function() {
         //app.receivedEvent('resume');
         RunApplication();
-        //window.open = cordova.InAppBrowser.open;
+        window.open = cordova.InAppBrowser.open;
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
